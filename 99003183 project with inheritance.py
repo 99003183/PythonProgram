935740393be418caef6044d933cd0173656d3371
99003183 <75420700+99003183@users.noreply.github.com>
2020-12-12 23:19:23 +0530

    Update and rename trial5.py to 99003183 project with inheritance.py

---
+++ 99003183 project with inheritance.py
@@ -0,0 +1,68 @@
+import sys
+
+
+class Mobiledata:
+    def __init__(self):
+        self.mobilename = ["Samsung S20FE", "Samsung M51", "Samsung A51"]
+        self.RAM = ["8", "6/8", "4/6/8"]
+        self.ROM = ["128", "128", "64/128"]
+        self.proce = ["Exy990", "Sd730G", "Exy9611"]
+        self.battery = [4500, 7000, 4000]
+        self.disp = [6.4, 6.7, 6.5]
+
+
+class Mobileinfo(Mobiledata):
+    def displayAvailablemobiles(self):
+        print("")
+        print("The mobiles we have in our library are as follows:")
+        cnt = 1
+        for mobile in self.mobilename:
+            print(cnt, ". ", mobile)
+            cnt += 1
+        print("")
+        print("""Want to view specifications of a mobile?
+        1. Yes
+        2. No""")
+        ch = int(input("Enter Choice: "))
+        if(ch == 1):
+            cho = int(input("Select the Mobile from list above: "))
+            if(cho > 3):
+                print("A request has already been sent to add details of",
+                      "this Mobile. Please give us some time to add")
+            else:
+                print("")
+                print("Mobile Name - ", self.mobilename[cho-1],
+                      "\nRAM \t\t- ", self.RAM[cho-1], "GB\nROM \t\t- ",
+                      self.ROM[cho-1], "GB\nProcessor \t- ", self.proce[cho-1],
+                      "\nBattery \t- ", self.battery[cho-1],
+                      "mAh\nDisplay \t- ", self.disp[cho-1], "inches")
+
+
+class mobileadd(Mobiledata):
+    def add1mobile(self):
+        mobilenam = input("Enter the name of the mobile you want to request: ")
+        self.mobilename.append(mobilenam)
+        print("\nThanks for requesting this Mobile. Please give us some time",
+              "to add it.\n\n")
+
+
+def main():
+    mp = Mobileinfo()
+    mp1 = mobileadd()
+    while 1:
+        print("""
+              ======Mobile Phone Guide by Akshay Sai Reddy G=======
+              1. Display all mobiles list
+              2. Request Admin to add a Mobile's Specification
+              0. Exit""")
+        choice = int(input("Enter Choice:"))
+        if choice == 1:
+            mp.displayAvailablemobiles()
+        elif choice == 2:
+            mp1.add1mobile()
+        elif choice == 0:
+            sys.exit()
+        else:
+            print("Invalid Input. Please select from below options.")
+
+main()
